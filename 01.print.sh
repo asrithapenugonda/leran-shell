775683d8613b9acdd2e99c5e11a7bc971fea388f
asrithapenugonda <asrithapenugonda16121@gmail.com>
2023-07-06 22:31:24 +0100

    color green,yellow,blue,magenta,cyan

--- 01.print.sh
+++ 01.print.sh
@@ -7,4 +7,8 @@ echo "**$ Ashi welcomes you all **$"
 #colour codes red,green,yellow,blue,magenta,cyan : 31,32,33,34,35,36
 
 echo -e "\e[31mthis is Ashi\e[0m"
-echo -e "/e[32m welcome back to my channel \e[0m"
+echo -e "\e[32m welcome back to my channel \e[0m"
+echo -e "\e[33m please \e[0m"
+echo -e "\e[34m share \e[0m"
+echo -e "\e[35m subscribe \e [0m"
+echo -e "\e[36m to my channel \e [0m"
