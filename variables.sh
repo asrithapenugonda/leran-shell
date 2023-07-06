8fe334aee45717b7f52577bd44b690bfee83b36d
asrithapenugonda <asrithapenugonda16121@gmail.com>
2023-07-06 22:52:54 +0100

    variable

--- variables.sh
+++ variables.sh
@@ -4,4 +4,8 @@ channel=jayashu
 
 # shellcheck disable=SC2086
 echo hi my name is ${name}
-echo welcome back to my $channel
+echo welcome back to my channel $channel
+
+date=$(date %F)
+
+echo "today's date is $date"
